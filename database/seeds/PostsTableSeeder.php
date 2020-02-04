4c4c272215b56e015f796516a4b2ba54527d807c
LukaszGr <lukasz.grychowski@gmail.com>
2020-02-04 05:54:36 +0000

    Update seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -22,6 +22,16 @@ class PostsTableSeeder extends Seeder
                 'title' => 'How to use a lightsaber efficiently',
                 'body' => $faker->text($maxNbChars = 2000),
             ],
+            [
+                'user_id' =>Arr::random($user),
+                'title' => 'How to deal with an distant father',
+                'body' => $faker->text($maxNbChars = 2000),
+            ],
+            [
+                'user_id' =>Arr::random($user),
+                'title' => 'How to lose a holochess game to a Wookie',
+                'body' => $faker->text($maxNbChars = 2000),
+            ],
         ]);
     }
 }
